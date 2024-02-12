e3c54227068a4064d4f04e9a5ab652e1a02edc21
awbibib <awbibib@gmail.com>
2024-02-13 05:14:38 +0700

    adding searchParams interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components'
 import { fuels, yearsOfProduction } from '@/constants';
 import { fetchCars } from '@/utils';
-import Image from 'next/image'
+import { SearchParams } from '@/types';
 
-export default async function Home({searchParams}) {
+
+export default async function Home({searchParams}: {searchParams: SearchParams}) {
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || '',
     year: searchParams.year || 2022,
